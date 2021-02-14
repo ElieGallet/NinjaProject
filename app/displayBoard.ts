5e0ecea905dc12a482ac63ccabb1ca4c92831ea5
g.Aime <aime.gallet@inodesign.fr>
2021-02-15 00:33:36 +0100

    add grid

--- app/displayBoard.ts
+++ app/displayBoard.ts
@@ -1,5 +1,37 @@
-const displayBoard = () =>{
-  throw new Error('to implement');
+import {Character,NonPlayerCharacter,PlayerCharacter} from "./Character";
+
+const displayBoard = (charList: Character[]) =>{
+	const lineGrid = 10;
+	const columnGrid = 10;
+	const BgRed = "\x1b[41m"
+	const BgGreen = "\x1b[42m"
+	const BgYellow = "\x1b[43m"
+	const BgBlue = "\x1b[44m"
+
+	let horToPrint:string = '';
+	let verToPrint:string = '';
+	let notEmpty=0;
+	
+	for(let x=0; x< lineGrid; x++){
+		horToPrint= '';
+		verToPrint= '';
+		for(let y=0; y< columnGrid; y++){
+			horToPrint += '----';
+			notEmpty =0;
+			charList.forEach(function(item, index, array) {
+				if((item.coordX == x)&&(item.coordY == y)){
+					verToPrint += '|XX|';
+					notEmpty = 1;
+				}
+			});
+			if(notEmpty == 0){
+					verToPrint += '|  |';
+			}
+		}
+		console.log(horToPrint);
+		console.log(verToPrint);
+	}
+	
 }
 
 export default displayBoard;
--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 import displayBoard from "./app/displayBoard";
+import {Character,NonPlayerCharacter,PlayerCharacter} from "./app/Character";
 
 const main = () =>{
-  displayBoard();
-  throw new Error('to implement');
+	let charBlue = new NonPlayerCharacter([]);
+	charBlue.coordX = 0;
+	charBlue.coordY = 0;
+
+	let charGreen = new NonPlayerCharacter([]);
+	charGreen.coordX = 9;
+	charGreen.coordY = 9;
+
+	let charYellow = new NonPlayerCharacter([]);
+	charYellow.coordX = 0;
+	charYellow.coordY = 9;
+
+	let charRed = new NonPlayerCharacter([]);
+	charRed.coordX = 9;
+	charRed.coordY = 0;
+
+	let charList: Character[] = [];
+
+	charList.push(charBlue);
+	charList.push(charGreen);
+	charList.push(charYellow);
+	charList.push(charRed);
+
+  displayBoard(charList);
 }
 
 main();
